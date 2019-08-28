8fbff1263336c40e3702f7a608cea393f9d1aa7d
Sainul Abidheen <sainul.abidheen@ibsplc.com>
2019-08-28 18:06:19 +0400

    Initial Committ

---
+++ TestingApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using TestingApp.API.Models;
+
+namespace TestingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options): base(options) {}
+        public DbSet<Value> Values { get; set; }
+    }
+}
+
+// public MyDbContext(DbContextOptions<MyDbContext> options) : base(options)
+// {
+// }
