7e634b85a9a606152ac8a626c60f145c29f84af6
Sainul Abidheen <sainul.abidheen@ibsplc.com>
2019-09-02 17:44:31 +0400

    SPA added back

---
+++ TestingApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  // @Input() valuesFromHome: any;
+  @Output() cancelRegister: EventEmitter<boolean> = new EventEmitter<boolean>();
+  model: any = {};
+
+  constructor(private authservice: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register() {
+    this.authservice.register(this.model).subscribe(() => {
+      console.log('registration successful');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+    console.log('cancelled');
+  }
+
+}
